d1bfba78b3e3b65f35940f4cd913fbe47d0b13c4
ArvinSamiei <samiei.arvin@gmail.com>
2020-11-09 19:03:04 +0330

    change type of actionTable field in ParseTable from ArrayList to list

--- MiniJava/src/main/java/parser/ParseTable.java
+++ MiniJava/src/main/java/parser/ParseTable.java
@@ -9,7 +9,7 @@ import java.util.*;
  * Created by mohammad hosein on 6/25/2015.
  */
 public class ParseTable {
-    private ArrayList<Map<Token,Action>> actionTable;
+    private List<Map<Token,Action>> actionTable;
     private ArrayList<Map<NonTerminal,Integer>> gotoTable;
     public ParseTable(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2,jsonTable.length()-2);
